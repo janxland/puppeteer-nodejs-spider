2aabd8c9318bc70508079872d73200a67d53c29a
Janx <janxland@gmail.com>
2023-04-09 21:36:44 +0800

    aaa

--- app.js
+++ app.js
@@ -8,40 +8,7 @@ process.on('uncaughtException', function (err) {
   console.log(err);
 });
 app.use(bodyParser.json());
-app.get('/scrape', async (req, res) => {
-const url = req.query.url || 'https://www.baidu.com';
-    const browser = await puppeteer.launch({
-        ignoreHTTPSErrors: true
-    });
-    const page = await browser.newPage();
-    page.setViewport({width:1200,height:1960})
-    await page.setBypassCSP(true);
-    await page.setExtraHTTPHeaders({
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
-    });
-    await page.goto(url, { waitUntil: 'networkidle2' });
-    
-    const content = await page.content();
-    page.screenshot();
-    await page.screenshot({ path: 'example.png' });
-    await browser.close();
-    res.send(content);
-});
-app.post('/scrape', async (req, res) => {
-    const url = req.body.url || 'https://www.baidu.com';
-    const browser = await puppeteer.launch({
-      ignoreHTTPSErrors: true
-    });
-    const page = await browser.newPage();
-    await page.setBypassCSP(true);
-    await page.setExtraHTTPHeaders({
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
-    });
-    await page.goto(url, { waitUntil: 'networkidle2' }); //load  domcontentloaded  networkidle2
-    const content = await page.content();
-    await browser.close();
-    res.send(content);
-});
+
 // Serverless 场景只能读写 /tmp 目录，所以这里需要指定上传文件的目录为 /tmp/upload
 const upload = multer({ dest: '/tmp/upload' });
 
